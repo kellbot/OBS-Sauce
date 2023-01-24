75169d9732d8074ed6a9e4607f2352601faf2990
kellbot <kellbot@gmail.com>
2023-01-24 10:47:53 -0500

    Set up for ZLR 3.3

--- pages/script.js
+++ pages/script.js
@@ -1,30 +1,26 @@
 let teamData = {
-	"event" : "WTRL TTT",
-	"course" : "Volcano Flats Reverse",
+	"event" : "ZRL 3.3",
+	"course" : "Innsbruckring",
 	"team": [
 		{
-			"name" : "Donna Hookway",
-			"zid" : 3990168
+			"name" : "Richard Badger-Taylor",
+			"zid" : 3478319
 		},
 		{
-			"name" : "Joanne Onions",
-			"zid" : 5257178
+			"name" : "Jason Blackbourn",
+			"zid" : 1354412
 		},
 		{
-			"name" : "Janice Rowan",
-			"zid" : 5257178
+			"name" : "Steve Knowles",
+			"zid" : 2570152
 		},
 		{
-			"name" : "Keen Fisk",
-			"zid" : 5257178
+			"name" : "Gaz Jones",
+			"zid" : 408281
 		},
 		{
-			"name" : "Brian Mudge",
-			"zid" : 5257178
-		},
-		{
-			"name" : "Ingo Meyer",
-			"zid" : 5257178
+			"name" : "James Grant",
+			"zid" : 1331113
 		}
 
 	]
@@ -46,73 +42,83 @@ $(function() {
 	$('#rider-template').hide();
 $('#course-name .scrollbox').html(teamData.course);
 
-async function setupDisplay(){
+async function setupSelf(){
 	const response = await fetch("../../../../api/athlete/stats/v1/self");
-	let userData = response.json();   	
-	
+	let userData = await response.json();   	
+	refreshStats(userData);
+	console.log(userData);
+}
+
+function refreshStats(athlete) {
+	let riderTime = toHoursAndMinutes(athlete.stats.elapsedTime);
+	$('#timer .infolabel').html(
+		riderTime.h + ":" + riderTime.m.toString().padStart(2,0) + ":" + riderTime.s.toString().padStart(2,0)
+	);
+	$('#current-power').html(athlete.state.power.toString() + "W - FTP: " + athlete.athlete.ftp);
+	//calculate zone from FTP
+	//=(B12/A12 * 100 - 45) / 15 * 16.6
+	let zonePCT = Math.floor(
+		(athlete.state.power / athlete.athlete.ftp * 100 - 45) / 15 * (100/6)
+		);
+	if (zonePCT < 0) zonePCT = 5;
+	if (zonePCT > 100) zonePCT = 100;
+	$('#current-hr').html(athlete.state.heartrate);
+	$('#powerzone .track').animate({'width': zonePCT + '%'}, 900);
+
+}
+
+
+async function populateLeaderboard(){
+	//manually set IDs
+	//or get random nearby IDs 
+	// const response = await fetch('../../../../api/nearby/v1');
+	// let neighborData = await response.json();
+	// teamData.raw = neighborData.slice(0,6);
+	// for (i in teamData.raw) {
+	// 	teamData.team[i].zid = teamData.raw[i].athleteId;
+	//   teamData.team[i].name = teamData.raw[i].athlete.fullname;
+	//   console.log(teamData.raw[i]);
+
+	// }
+
+
+	for (i in teamData.team) {
+		let rider = teamData.team[i];
+		let riderLine = $("<li data-zid='"+rider.zid+"'><span class='rider-name'>"+rider.name+
+			"</span> <span class='monotime'>--:--</span></li>");
+		$('#roster ul').append(riderLine);
+
+	}
 
 }
 
 function updateDisplay(){
 
-	//load in my data
-	let mainRider;
+	let riderLines = [];
 
-	fetch('../../../../api/athlete/stats/v1/self')
-		.then((response) => response.json())
-	    .then((json) => { 
-	    	mainRider = json;
-	    	let riderTime = toHoursAndMinutes(mainRider.stats.elapsedTime);
-	    	$('#timer .infolabel').html(
-	    		riderTime.h + ":" + riderTime.m.toString().padStart(2,0) + ":" + riderTime.s.toString().padStart(2,0)
-	    	);
-	    	$('#current-power').html(mainRider.state.power.toString());
-	    	//calculate zone from FTP
-	    	//=(B12/A12 * 100 - 45) / 15 * 16.6
-	    	let zonePCT = (mainRider.state.power / mainRider.athlete.ftp * 100-45) / 15 * 16.6;
-	    	$('#powerzone .track')
-	    	.animate('width: ' + zonePCT.toString() + '%', 700);
-	    });
-
-
-
-	fetch('../../../../api/nearby/v1')
+	for (i in teamData.team) {
+			
+			let rider = teamData.team[i];
+			fetch('../../../../api/athlete/stats/v1/' + rider.zid)
 			    .then((response) => response.json())
 			    .then((json) => {
-			    	teamData.team = json.slice(0,6);
-					let riderLines = [];
-					let riderList = $("<ul></ul>");
-
-					for (i in teamData.team) {
-							
-							let riderLine = $("<li></li>");
-
-							let riderTemplate = $.parseHTML('<span class="rider-name">Kelly Maguire</span> <span class="monotime">34:22</span>');
-							riderLine.html(riderTemplate);
-							let rider = teamData.team[i];
-							fetch('../../../../api/athlete/stats/v1/' + rider.athleteId)
-							    .then((response) => response.json())
-							    .then((json) => {
-							    	//console.log(json);
-							    	let gap = toHoursAndMinutes(json.gap);
-							    	riderLine.children('.monotime').html(gap.m + ":" + gap.s.toString().padStart(2,0));
-							    	
-									riderLine.children('.rider-name').html(json.athlete.fullname);
-							    });
-							
-
-							riderLines[i] = riderLine;
-							riderList.append(riderLine);
-					}
-
-					$('#roster ul').replaceWith(riderList);
-					
-
+			    	let riderLine = $("li[data-zid='"+rider.zid+"']").first();
+			    	let gap = toHoursAndMinutes(json.gap);
+			    	riderLine.children('.monotime').html(gap.m + ":" + gap.s.toString().padStart(2,0));
+			    	
+					riderLine.children('.rider-name').html(rider.name);
+					console.log(json);
 			    });
+			
 
+	}
+
+					
 }
 
-setupDisplay();
+populateLeaderboard();
+
+setInterval(setupSelf, 1000);
 //setInterval(updateDisplay, 1000);
 //updateDisplay();	
 });
